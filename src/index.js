695dd3d3fb73d9b26e123a7f58ae7673b6276787
Jay Honnold <jayhonnold@gmail.com>
2019-09-14 09:04:27 -0500

    Handle room deletion on Host disconnect

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ const cors = require('cors');
 const authorizer = require('./auth');
 const store = require('./store');
 const { registerUser, disconnectUser } = require('./ducks/users');
-const { teardownUser } = require('./ducks/roomConnections');
+const { teardownUser, teardownRoom } = require('./ducks/roomConnections');
+const { deleteRoom } = require('./ducks/rooms');
 const getRoom = require('./util/getRoom');
 
 const app = express();
@@ -25,15 +26,23 @@ io.on('connect', client => {
   }
 
   client.on('disconnect', () => {
-    const { roomConnections } = store.getState();
+    const { roomConnections, rooms } = store.getState();
     const changingRooms = new Set(roomConnections
         .filter(c => c.userId === client.id)
         .map(c => c.roomId));
 
+    const deletedRooms = new Set(Object.values(rooms).filter(r => r.hostId === client.id).map(r => r.roomId));
+
     store.dispatch(disconnectUser(client.id));
     store.dispatch(teardownUser(client.id));
-
     changingRooms.forEach(r => io.emit('room updated', getRoom(r)));
+
+    deletedRooms.forEach(r => {
+      store.dispatch(deleteRoom(r));
+      store.dispatch(teardownRoom(r));
+
+      io.emit('room deleted', { roomId: r });
+    });
   });
 });
 
